64b0c91f94372e0edfbf1a72fbae4313ab6ed1ea
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-04-07 14:00:44 +0200

    Finalisation du style pour la section ateliers

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
     get_header();
     
     echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+
     include 'templates-part/ateliers-loop.php';
-    // echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
     include 'templates-part/instruments-loop.php';
     include 'templates-part/actualites-loop.php';
     include 'templates-part/contact-form.php';
--- templates-part/ateliers-loop.php
+++ templates-part/ateliers-loop.php
@@ -14,7 +14,9 @@
                 <?php the_excerpt(); ?>
                 <a class="primary-button" href=<?php the_permalink(); ?>>En savoir plus</a>
             </div>
-            <?php the_post_thumbnail(); ?>
+            <div class="atelier-image-container">
+                <div class="atelier-image"><?php the_post_thumbnail(); ?></div>
+            </div>
         </div>
 
         <?php endwhile; wp_reset_query(); ?>
