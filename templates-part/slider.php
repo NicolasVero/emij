73cb4acaa5d83da35ae61b52de2fb537cd81def4
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-04-11 14:58:39 +0200

    Ajout du fichier head

--- templates-part/slider.php
+++ templates-part/slider.php
@@ -6,19 +6,21 @@
         while ($query->have_posts()) :
             $query->the_post();
         ?>   
-    <div class="slider">
-
-            <h2><?php the_title(); ?></h2>			
+        
+        <div class="slider">
+            <?php include '../assets/svg/logo.php'; ?>
+            <h2><?php the_title(); ?></h2>
+            <br><br><br>			
             <a class="primary-button" href=<?php the_permalink(); ?>>En savoir plus</a>
             <figure><?php 
+                $image = get_field('image-slider');
+                
+                if( !empty( $image ) ): ?>
+                    <img src="<?= esc_url($image['url']); ?>" alt="<?= esc_attr($image['alt']); ?>" />
+                <?php endif; ?>
+            
+            </figure>
+        </div>
+    <?php endwhile; wp_reset_query(); ?>
 
-                if( have_rows('image-slider') ) {
-                    while( have_rows('image-slider') ) { 
-                        get_sub_field('');
-                    }
-                }
-            ?></figure>
 </div>
-
-        <?php endwhile; wp_reset_query(); ?>
-            </div>
