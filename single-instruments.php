1b9663756f4c378deac7a344f7c62a45e1880559
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-04-17 14:43:36 +0200

    Correction des couleurs de la page instrument

--- single-instruments.php
+++ single-instruments.php
@@ -9,23 +9,15 @@ include 'template-parts/instruments/slider.php';
 
 ?>
 	<div class="post">
-	<div class="instruments">
-        <?php 
-        the_content(); 
-        
-        
-        
-        // var_dump(get_the_content());
-        ?> 
+        <div class="instruments">
+            <?php the_content(); ?> 
+        </div>
+
+        <?php
+            include 'template-parts/instruments/professeurs-loop.php';
+            include 'template-parts/index/contact-form-bis.php';
+            include 'template-parts/instruments/nous-trouver.php';
+        ?>
     </div>
-
-	
-<?php
-    include 'template-parts/instruments/professeurs-loop.php';
-    include 'template-parts/index/contact-form-bis.php';
-    include 'template-parts/instruments/nous-trouver.php';
-?>
-
-</div>
-			
+                
 <?php endwhile; get_footer(); ?>
