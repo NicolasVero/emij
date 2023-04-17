5909a08db31ef4f37ef59d7d37761dedbcc38210
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-04-17 11:16:01 +0200

    Ajout du style pour la section nous trouver

--- single-instruments.php
+++ single-instruments.php
@@ -22,22 +22,10 @@ include 'template-parts/instruments/slider.php';
 	
 <?php
     include 'template-parts/instruments/professeurs-loop.php';
-    echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
     include 'template-parts/index/contact-form-bis.php';
-    
-
-    ?>
-
-<!-- //TODO A enlever ? -->
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3680.712025900769!2d1.0071511218552984!3d49.28290701451484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e11ed67ff583a1%3A0x9406924f7a782929!2sI.U.T%20d&#39;Elbeuf!5e0!3m2!1sfr!2sfr!4v1680598393911!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-
+    include 'template-parts/instruments/nous-trouver.php';
+?>
 
 </div>
 			
-<?php endwhile; 
-
-
-    get_footer();
-
-?>
+<?php endwhile; get_footer(); ?>
---
+++ template-parts/instruments/nous-trouver.php
@@ -0,0 +1,4 @@
+<div class="nous-trouver">
+    <span><p>Nous trouver</p></span>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3680.712025900769!2d1.0071511218552984!3d49.28290701451484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e11ed67ff583a1%3A0x9406924f7a782929!2sI.U.T%20d&#39;Elbeuf!5e0!3m2!1sfr!2sfr!4v1680598393911!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
