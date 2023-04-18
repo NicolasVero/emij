333ee5f893fb2b8fe4489e5ada14f31fc6850e45
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-04-18 10:47:15 +0200

    Un professeur n'apparait plus que sur les cours qu'il dispense

--- template-parts/instruments/professeurs-loop.php
+++ template-parts/instruments/professeurs-loop.php
@@ -3,17 +3,24 @@
     
     $query = new WP_Query( array( 'post_type' => 'professeurs') ); 
 
+    global $post;
+    $titre_article = $post->post_name;
 
     while ($query->have_posts()) {
         $query->the_post();
-    ?>   
         
+        if(have_rows('cours')) :
+            while( have_rows('cours') ) :
+                $nom_categorie = the_row();
+
+                if($nom_categorie == $titre_article) :
+    ?>   
     <div class="professeur"> 
         <?php the_post_thumbnail(); ?>
         <p><?php the_title(); ?></p>			
         <p><?php the_field('instruments'); ?></p>
     </div>
-    <?php } 
+    <?php  endif; endwhile;endif; } 
 
 
     // $query = new WP_Query( array( 'post_type' => 'instruments') ); 
