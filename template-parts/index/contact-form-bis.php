85faa51ddbb5ca783743028c7001ab121ab27f9d
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-04-20 18:43:25 +0200

    Ajustement du slider responsive

--- template-parts/index/contact-form-bis.php
+++ template-parts/index/contact-form-bis.php
@@ -1,9 +1,7 @@
-<section>
 <div class="contact-form-bis col-12">
 
     <h2>RESTEZ INFORMÉ</h2>
     <p>lorem ipsum dolor eres</p>
 
-        <?= do_shortcode('[contact-form-7 id="48" title="Formulaire de contact"]'); ?>
-    </div>
-</section>
+    <?= do_shortcode('[contact-form-7 id="48" title="Formulaire de contact"]'); ?>
+</div>
