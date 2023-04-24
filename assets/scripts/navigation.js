49cc6cb435c4e624ac187f42265c56534c8503c4
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-04-24 14:50:11 +0200

    Ajout du menu entierement fonctionnel

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,14 +1,21 @@
-const bouton = document.getElementsByClassName('open-navigation')[0];
-console.log(bouton);
+const bouton_open  = document.getElementsByClassName('open-navigation')[0];
+const bouton_close = document.getElementsByClassName('close-navigation')[0];
+console.log(bouton_open);
+console.log(bouton_close);
 const navigation = document.getElementById('navigation-container');
 console.log(navigation);
 
-bouton.addEventListener('click', () => {
-    console.log("coucou")
-})
+// bouton.addEventListener('click', () => {
+//     console.log("coucou")
+// })
 
 
-// bouton.addEventListener('click', function() {
-//     console.log('clic');
-//     navigation.classList.toggle('active');
-//   });
+bouton_open.addEventListener('click', function() {
+    console.log('clic');
+    navigation.classList.add('active');
+});
+
+bouton_close.addEventListener('click', function() {
+    console.log('clic');
+    navigation.classList.remove('active');
+  });
