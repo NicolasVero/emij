b2b7d72295027ba3b01f6f8e9da17654526a9382
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-05-24 13:57:26 +0200

    finalisation du style des formulaires

--- page-contact.php
+++ page-contact.php
@@ -1,20 +1,10 @@
 <?php get_header() ?>
 
-<style>
-
-
-
-</style>
-
-
-
-
     <div class="formulaire-contact">
         <h2>Formulaire de contact</h2>
         <p>lorem ipsum dolor eres</p>
         
         <?= do_shortcode('[contact-form-7 id="268" title="Formulaire de contact"]'); ?>
     </div>
-
 
 <?php get_footer() ?>
