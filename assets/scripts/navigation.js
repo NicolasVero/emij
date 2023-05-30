6af4adfa2bcb45789fdddaf1bdcf2802cda43630
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-05-30 14:23:15 +0200

    changement animation slider

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,6 +1,6 @@
 const bouton_open  = document.getElementsByClassName('open-navigation')[0];
 const bouton_close = document.getElementsByClassName('close-navigation')[0];
-const navigation = document.getElementById('navigation-container');
+const navigation   = document.getElementById('navigation-container');
 
 bouton_open.addEventListener('click', function() {
     navigation.classList.add('open');
--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -1,16 +1,4 @@
 slider_slide();
-// slider_fade();
-
-
-
-
-
-
-
-
-
-
-
 
 function slider_slide() {
 
@@ -19,16 +7,20 @@ function slider_slide() {
 
         const slideCount = $('.sliders li').length; 
         const animationContentDuration = 800; 
+        const animationImageDuration = 600; 
         const animationBackgroundDuration = 600; 
+        const sliderMargin = 4;
          
         var slideWidth = $('.sliders li').outerWidth(); 
         var slideIndex = 0; 
+        var is_blocked = false;
         
-        // $('.sliders-background').css('background', 'dddddd').fadeIn('slow');
 
         function update_slider_position() {
-            var leftPosition = -slideIndex * slideWidth - 4 * slideIndex;
-            $('.sliders').animate({ 'left': leftPosition }, animationContentDuration);
+            var leftPosition = -slideIndex * slideWidth - sliderMargin * slideIndex;
+            // $('.sliders').animate({ 'left': leftPosition }, animationContentDuration);
+            $('.sliders .container > div').animate({ 'left': leftPosition }, animationContentDuration);
+            $('.sliders .container figure').animate({ 'left': leftPosition }, animationImageDuration);
         }
 
         function incremente_slider() {
@@ -53,7 +45,7 @@ function slider_slide() {
 
             
             $('.sliders-background').fadeOut(animationBackgroundDuration, function() {
-                $(this).removeClass().addClass('sliders-background ' + degrades[slideIndex]);
+                $(this).removeClass().addClass('sliders-background ' + degrades[slideIndex % degrades.length]);
                 $(this).fadeIn(animationBackgroundDuration);
             });
 
@@ -65,13 +57,12 @@ function slider_slide() {
         });
         
 
-
-
         $('.prev-button').click(function() {
             
             decremente_slider();
             update_slider_position();
             change_color();
+            slider_blocked();
         });
         
         $('.next-button').click(function() {
@@ -79,70 +70,25 @@ function slider_slide() {
             incremente_slider();
             update_slider_position();
             change_color();
+            slider_blocked();
         });
-    });
-}
 
 
+        function slider_blocked() {
 
+            is_blocked = true;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function slider_fade() {
-   
-    $(document).ready(function() {
-        const slideCount = $('.sliders li').length;
-        const animationDuration = 500;
-        
-        var slideIndex = 0;
-        
-        function fadeOutCurrentSlide(old) {
-            $('.sliders li').eq(old).hide().css('opacity', 1);
+            setTimeout( function() {
+                is_blocked = false;
+            }, 8000);
         }
-        
-        function fadeInNextSlide() {
-            $('.sliders li').eq(slideIndex).fadeIn(animationDuration);
-        }
-        
-        $('.prev-button').click(function() {
-            
-            old = slideIndex;
 
-            slideIndex--;
-            if (slideIndex < 0)
-            slideIndex = slideCount - 1;
-            
-            fadeInNextSlide();
-            fadeOutCurrentSlide(old);
-            
-        });
-        
-        $('.next-button').click(function() {
-            
-            old = slideIndex;
-
-            slideIndex++;
-            if (slideIndex >= slideCount)
-            slideIndex = 0;
-            
-            fadeInNextSlide();
-            fadeOutCurrentSlide(old);
-        });
+        setInterval( function() {
+            if(!is_blocked) {
+                incremente_slider();
+                update_slider_position();
+                change_color();
+            }
+        }, 5000)
     });
 }
