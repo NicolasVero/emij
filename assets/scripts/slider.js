280e69331e7ef5bd8fb1a58521c3d143fe95e26c
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-05-30 14:58:37 +0200

    correction bug animation slider (changement page)

--- assets/scripts/slider.js
+++ assets/scripts/slider.js
@@ -5,20 +5,23 @@ function slider_slide() {
     
     $(document).ready(function() {
 
+        // CONSTANTES 
         const slideCount = $('.sliders li').length; 
         const animationContentDuration = 800; 
         const animationImageDuration = 600; 
         const animationBackgroundDuration = 600; 
         const sliderMargin = 4;
          
+        // VARIABLES 
         var slideWidth = $('.sliders li').outerWidth(); 
         var slideIndex = 0; 
         var is_blocked = false;
+        var visibilityChangePaused = false;
         
 
+        // FONCTIONS
         function update_slider_position() {
             var leftPosition = -slideIndex * slideWidth - sliderMargin * slideIndex;
-            // $('.sliders').animate({ 'left': leftPosition }, animationContentDuration);
             $('.sliders .container > div').animate({ 'left': leftPosition }, animationContentDuration);
             $('.sliders .container figure').animate({ 'left': leftPosition }, animationImageDuration);
         }
@@ -43,15 +46,47 @@ function slider_slide() {
                 "degrade-3"
             ];
 
-            
             $('.sliders-background').fadeOut(animationBackgroundDuration, function() {
                 $(this).removeClass().addClass('sliders-background ' + degrades[slideIndex % degrades.length]);
                 $(this).fadeIn(animationBackgroundDuration);
             });
+        }
 
+        function slider_blocked() {
+
+            is_blocked = true;
+
+            setTimeout( function() {
+                is_blocked = false;
+            }, 8000);
+        }
+
+        function pause_animation() {
+            if (!visibilityChangePaused && !is_blocked) {
+                $('.sliders .container > div, .sliders .container figure').stop(true, true); 
+                visibilityChangePaused = true;
+            }
+        }
+    
+        function resume_animation() {
+            if (visibilityChangePaused && !is_blocked) {
+                update_slider_position(); 
+                visibilityChangePaused = false;
+            }
         }
 
         
+
+        // ECOUTEURS D'EVENEMENTS
+        document.addEventListener("visibilitychange", function() {
+            if (document.visibilityState === 'hidden') {
+                pause_animation();
+            } else {
+                resume_animation();
+            }
+        });
+
+
         $(window).on('resize', function() {
             slideWidth = $('.sliders li').outerWidth();
         });
@@ -64,6 +99,7 @@ function slider_slide() {
             change_color();
             slider_blocked();
         });
+    
         
         $('.next-button').click(function() {
             
@@ -74,21 +110,13 @@ function slider_slide() {
         });
 
 
-        function slider_blocked() {
-
-            is_blocked = true;
-
-            setTimeout( function() {
-                is_blocked = false;
-            }, 8000);
-        }
-
         setInterval( function() {
             if(!is_blocked) {
                 incremente_slider();
                 update_slider_position();
                 change_color();
             }
-        }, 5000)
+        }, 5000);
+
     });
 }
